c8229a874d854c52fff1d040ef5fc16cd6059f9a
unknown <bruno.alveolar@gmail.com>
2023-08-29 21:25:06 -0300

    voltando para o estado do Victor

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gererenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Inserindo uma linha sem avisar")
-console.log("console log indicando modificacoes do Victor")
