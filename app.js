36c5780d9a817070797e06d4bfb0df495c851874
unknown <bruno.alveolar@gmail.com>
2023-08-29 21:19:34 -0300

    console log indicando modificacoes do Victor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gererenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Inserindo uma linha sem avisar")
+console.log("console log indicando modificacoes do Victor")
