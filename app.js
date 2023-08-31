71706e84e9963433ed0e1064acb6759a6f0164f9
unknown <bruno.alveolar@gmail.com>
2023-08-30 22:15:08 -0300

    Treinando o commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gererenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Inserindo uma linha sem avisar")
+console.log("Testanto meu conhecimento")
+
